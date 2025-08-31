56b27e7789e167261d8079aa68661e3f53b2af6c
Ahmed Mohammed <109735125+ahmedmoahmed00@users.noreply.github.com>
2025-08-31 19:22:03 +0300

    edit bug in UserProfile

--- src/components/ProfileInformation.jsx
+++ src/components/ProfileInformation.jsx
@@ -3,12 +3,12 @@ import FormInput from "../forms/FormInput";
 import UserProfileForm from "../forms/UserProfileForm";
 
 function ProfileInformation() {
-  const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(true);
   return (
     <div className="p-6 border  border-gray-200 w-full rounded-xl flex flex-col gap-6 shadow-sm">
       <header className="flex justify-between items-center ">
         <h2 className="leading-none font-semibold">Profile Information</h2>
-        {!disabled && (
+        {disabled && (
           <>
             <button
               onClick={() => setDisabled((prev) => !prev)}
@@ -27,13 +27,16 @@ function ProfileInformation() {
         </form>
       </div>
       <div className="flex items-center gap-4">
-        {disabled && (
+        {!disabled && (
           <>
-            <button className="cursor-pointer py-2 px-4 text-sm text-white bg-black rounded-md hover:bg-black/80">
+            <button
+              onClick={() => setDisabled(true)}
+              className="cursor-pointer py-2 px-4 text-sm text-white bg-black rounded-md hover:bg-black/80"
+            >
               Save Changes
             </button>
             <button
-              onClick={() => setDisabled(false)}
+              onClick={() => setDisabled(true)}
               className="cursor-pointer py-2 px-4 text-sm text-black bg-white rounded-md hover:bg-black/3 border border-gray-300"
             >
               Cancel
